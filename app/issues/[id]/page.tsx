33ac67076ffb39f2034af5554f2ae6aae9864a2e
Levis155 <levismbui@gmail.com>
2025-10-13 06:57:39 +0300

    Optimize performance cache

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -8,24 +8,23 @@ import EditIssueButton from "./EditIssueButton";
 import IssueDetails from "./IssueDetails";
 import DeleteIssueButton from "./edit/DeleteIssueButton";
 import AssigneeSelect from "./AssigneeSelect";
-import { Metadata } from "next";
+import { cache } from "react";
 
 interface Props {
   params: { id: string };
 }
 
+const fetchUser = cache((issueId: number) =>
+  prisma.issue.findUnique({ where: { id: issueId } })
+);
+
 const IssueDetailPage = async ({ params }: Props) => {
   const session = await getServerSession(authOptions);
-  const issue = await prisma.issue.findUnique({
-    where: {
-      id: parseInt(params.id),
-    },
-  });
+  const issue = await fetchUser(parseInt(params.id));
 
   if (!issue) {
     notFound();
   }
-
   await delay(2000);
 
   return (
@@ -47,9 +46,7 @@ const IssueDetailPage = async ({ params }: Props) => {
 };
 
 export async function generateMetadata({ params }: Props) {
-  const issue = await prisma.issue.findUnique({
-    where: { id: parseInt(params.id) },
-  });
+  const issue = await fetchUser(parseInt(params.id));
 
   return {
     title: issue?.title,
